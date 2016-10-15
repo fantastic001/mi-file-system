483327e499d7ed9a3d3865fd47913849d192a6b6
Nikola Bebic <nikola.bebic99@gmail.com>
2016-10-15 23:18:52 +0100

    change the mknod function

--- fuse/mi_mknod.cpp
+++ fuse/mi_mknod.cpp
@@ -4,13 +4,27 @@
 
 int mi_mknod(const char* name, mode_t mode, dev_t dev)
 {
+    mi_log(name);
+    mi_log("mknod()\n");
+
+    char base[256] = {0};
+    char fn[256] = {0};
+
+    mi_split_path(name, base, fn);
+    
+
     mi_context* ctx = mi_get_context();   
+    node* folder = ctx->root;
+    if (base[0]!=0) 
+    {
+        folder = mi_get_destination(base);
+    }
     node * nxt = ctx->mem->create_node(
-            const_cast<char*>(name), 
+            const_cast<char*>(fn), 
             false,
-            ctx->curr,
-            ctx->curr,
-            ctx->curr->younger);
-    ctx->curr->younger = nxt;
+            folder,
+            folder,
+            folder->younger);
+    folder->younger = nxt;
     return 0;
 }
