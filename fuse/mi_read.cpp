c0b4f0d62dc833e8fcfb60e9e3cf641a9d4ef7f1
Nikola Bebic <nikola.bebic99@gmail.com>
2016-10-16 15:38:17 +0100

    fix write and read. they work, FUSE doesn't.

--- fuse/mi_read.cpp
+++ fuse/mi_read.cpp
@@ -4,13 +4,11 @@
 
 int mi_read (const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi) 
 {
-	mi_log("read() ");
-	mi_log(path);
-	mi_log("\n");
-        //node *curr = fi->fd;
+        int oldsize = size;
         int readed = 0; // znam da je nepravilno ali ne moze read
         node *curr = mi_get_destination(path);
         memory *part = mi_get_context()->mem;
+	mi_log("\nread(%s, **, %i, %i, **): '", path, size, offset);
         if (curr->folder) return -EISDIR;
     
         int frsz = fr_mem * 1024 - 2 - 4; // fr_mem - sizeof(short) - sizeof(int);
@@ -22,15 +20,23 @@ int mi_read (const char *path, char *buf, size_t size, off_t offset, struct fuse
             for (int i = 0; i < frsz && sz; i++)
             {
                 if (offset) --offset;
-                else *buf = *((char*)(part->mem[from_fr] + 2 + i));
-                ++buf;
-                ++readed;
-                --size;
-                if (!size) return readed;
+                else 
+                {
+                    *buf = *((char*)(part->mem[from_fr] + 2 + i));
+                    mi_log("%02x ", *buf);
+                    ++buf;
+                    ++readed;
+                    --size;
+                    if (!size) {
+                        mi_log("' readed %i\n", readed);
+                        return readed;
+                    }
+                }
                 sz--;
             }
     
             from_fr = *((int*)( (char*)(part->mem[from_fr] + fr_mem * 1024 - 4)) );
         }
+        mi_log("' readed [eof] %i\n", readed);
         return readed;
 }
--- fuse/mi_write.cpp
+++ fuse/mi_write.cpp
@@ -4,12 +4,11 @@
 
 int mi_write (const char *path, const char *buff, size_t size, off_t offset, struct fuse_file_info *fi) 
 {
-	mi_log("write() ");
-	mi_log(path);
-	mi_log("\n");
+        mi_log("write('%s', **, %i, %i, **);", path, size, offset);
         if (offset != 0) mi_log("e sad jebem li ga ne umem pisat iz sredine\n   - B\n");
         node *curr = mi_get_destination(path);
         memory *part = mi_get_context()->mem;
+        curr->start = part->fff_fragment();
         part->add_extern_file(
                 curr, 
                 const_cast<char*>(buff), // obelezavajte ulazne bafere kao const 
